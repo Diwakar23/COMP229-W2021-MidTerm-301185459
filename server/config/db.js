eaaf16b3317628221b213294eb45659f0af6e383
Diwakar23 <diwakar232496@gmail.com>
2021-03-06 23:44:43 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb+srv://Diwakar:Diwakar@23@mongodbserver.jtk9q.mongodb.net/user_collection?retryWrites=true&w=majority"
+  "URI":"mongodb+srv://Diwakar:Diwakar@23@mongodbserver.cq2dc.mongodb.net/book_store?retryWrites=true&w=majority"
+}
